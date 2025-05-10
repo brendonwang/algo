88488b88c0870b3568f81f073cca2248e7b7171a
brendon.wang <brendon.wang@gmail.com>
2025-05-10 14:48:01 -0700

    polynomial and hld

---
+++ library/polynomial/poly.h
@@ -0,0 +1,85 @@
+#pragma once
+
+struct poly {
+	vector<complex<long double>> coef;
+
+	poly() {}
+
+	static int next_pow_2(int n) {
+		int p = 1;
+		while (p < n) p <<= 1;
+		return p;
+	}
+
+	template<typename T>
+	explicit poly(const vector<T> &real, int m = -1) {
+		if (m == -1) m = real.size();
+		int n = next_pow_2(m);
+		coef.resize(n);
+		for (int i = 0; i < real.size(); ++i) coef[i] = real[i];
+		for (int i = real.size(); i < n; ++i) coef[i] = complex<long double>(0);
+	}
+
+	explicit poly(int m) {
+		int n = next_pow_2(m);
+		coef.resize(n, complex<long double>(0));
+	}
+
+	size_t size() const {
+		return coef.size();
+	}
+
+	complex<long double> &operator[](int idx) {
+		return coef[idx];
+	}
+
+
+	const complex<long double> &operator[](int idx) const {
+		return coef[idx];
+	}
+
+	static size_t reverseBits(size_t num, int log2n) {
+		size_t reversed = 0;
+		for (int i = 0; i < log2n; ++i)
+			if (num & (1 << i)) reversed |= (1 << (log2n - 1 - i));
+		return reversed;
+	}
+
+	friend void fft(poly &a, bool invert = false) {
+		int n = a.size();
+		int log2n = log2(n);
+		for (int i = 0; i < n; ++i) {
+			int j = reverseBits(i, log2n);
+			if (i < j) swap(a[i], a[j]);
+		}
+		for (int len = 2; len <= n; len *= 2) {
+			long double angle = 2 * acos(-1) / len * (invert ? -1 : 1);
+			complex<long double> wlen(cos(angle), sin(angle));
+			for (int i = 0; i < n; i += len) {
+				complex<long double> w(1);
+				for (int j = 0; j < len / 2; ++j) {
+					complex<long double> u = a[i + j];
+					complex<long double> v = a[i + j + len / 2] * w;
+					a[i + j] = u + v;
+					a[i + j + len / 2] = u - v;
+					w *= wlen;
+				}
+			}
+		}
+		if (invert) {
+			for (auto &x: a.coef) x /= n;
+		}
+	}
+
+	friend poly operator*(poly &a, poly &b) {
+		int n = next_pow_2(a.size() + b.size() - 1);
+		poly aval(a.coef, n), bval(b.coef, n);
+		fft(aval);
+		fft(bval);
+		for (int i = 0; i < n; ++i) aval[i] *= bval[i];
+		fft(aval, true);
+		aval.coef.resize(a.size() + b.size() - 1);
+		for (auto &x: aval.coef) x = complex<long double>(round(x.real()), 0);
+		return aval;
+	}
+};
---
+++ library/tree/hld.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "../data-structures/segtree.h"
+
+template<typename T = int>
+struct hld {
+	int n;
+	vector<vector<int>> &adj;
+	vector<int> parent, depth, heavy, head, pos;
+	segtree<T> st;
+	int t = 0, unit;
+	function<T(T, T)> f;
+	hld(vector<vector<T>> &G, T unit, function<T(T, T)> f)
+			: n(G.size()), adj(G),
+			  parent(n, -1), depth(n), heavy(n, -1),
+			  head(n), pos(n), unit(unit), f(f), st(n, unit, f) {
+		dfs(0);
+		decompose(0, 0);
+	}
+	hld(vector<vector<int>> &G, vector<T> &vals, T unit, function<T(T, T)> f)
+			: n(G.size()), adj(G),
+			  parent(n, -1), depth(n), heavy(n, -1),
+			  head(n), pos(n), unit(unit), f(f), st(n, unit, f) {
+		dfs(0);
+		decompose(0, 0);
+		for (int i = 0; i < n; ++i) {
+			st.set(pos[i], vals[i]);
+		}
+	}
+	int dfs(int v) {
+		int sz = 1, maxsz = 0;
+		for (auto &u: adj[v]) {
+			if (u == parent[v]) continue;
+			parent[u] = v;
+			depth[u] = depth[v] + 1;
+			int childsz = dfs(u);
+			if (childsz > maxsz) {
+				heavy[v] = u;
+				maxsz = childsz;
+			}
+			sz += childsz;
+		}
+		return sz;
+	};
+	void decompose(int v, int h) {
+		head[v] = h;
+		pos[v] = t++;
+		if (heavy[v] != -1) {
+			decompose(heavy[v], h);
+		}
+		for (auto &u: adj[v]) {
+			if (u == parent[v] || u == heavy[v]) continue;
+			decompose(u, u);
+		}
+	}
+
+	template<typename F>
+	void query_path(int u, int v, F op) {
+		while (head[u] != head[v]) {
+			if (depth[head[u]] < depth[head[v]]) swap(u, v);
+			op(pos[head[u]], pos[u]);
+			u = parent[head[u]];
+		}
+		if (depth[u] > depth[v]) swap(u, v);
+		op(pos[u], pos[v]);
+	}
+
+	void update(int v, T val) {
+		st.update(pos[v], val);
+	}
+	void set(int v, T val) {
+		st.set(pos[v], val);
+	}
+
+	T query(int a, int b) {
+		T res = unit;
+		query_path(a, b, [&](auto x, auto y) {
+			res = f(res, st.query(x, y + 1));
+		});
+		return res;
+	}
+};
