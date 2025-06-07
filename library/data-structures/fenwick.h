4f52997b8c789f5066db1e0aeeb4045b9f344793
brendon.wang <brendon.wang@gmail.com>
2025-06-06 22:53:08 -0700

    add fenwick tree

--- library/data-structures/fenwick.h
+++ library/data-structures/fenwick.h
@@ -1,2 +1,27 @@
 #pragma once
 
+template<typename T>
+struct fenwick {
+	vector<T> tree;
+	int n;
+
+	fenwick(int n) : n(n), tree(n + 1, 0) {}
+
+	void update(int i, T val) {
+		for (++i; i <= n; i += i & -i) {
+			tree[i] += val;
+		}
+	}
+
+	T query(int i) {
+		T sum = 0;
+		for (++i; i > 0; i -= i & -i) {
+			sum += tree[i];
+		}
+		return sum;
+	}
+
+	T query(int l, int r) {
+		return query(r) - query(l - 1);
+	}
+};
