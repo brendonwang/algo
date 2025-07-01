c3a1e2b7255024c2c2db143c12c5474b655ee36d
brendon.wang <brendon.wang@gmail.com>
2025-07-01 11:56:52 -0700

    finish poly class

--- library/polynomial/poly.h
+++ library/polynomial/poly.h
@@ -1,86 +1,63 @@
 #pragma once
 
+template<typename T = int, typename U = long double>
 struct poly {
-	vector<complex<long double>> coef;
-
+	vector<T> coef;
 	poly() {}
-
-	static int next_pow_2(int n) {
-		int p = 1;
-		while (p < n) p <<= 1;
-		return p;
+	static inline int next_pow_2(int n) { return 1 << (32 - __builtin_clz(n - 1)); }
+	template<typename V>
+	explicit poly(const vector<V> &real) {
+		coef.resize(real.size());
+		for (int i = 0; i < real.size(); ++i) { coef[i] = real[i]; }
 	}
+	explicit poly(int n) { coef.resize(n, 0); }
+	inline size_t size() const { return coef.size(); }
+	inline T &operator[](int idx) { return coef[idx]; }
+	inline const T &operator[](int idx) const { return coef[idx]; }
 
-	template<typename T>
-	explicit poly(const vector<T> &real, int m = -1) {
-		if (m == -1) m = real.size();
-		int n = next_pow_2(m);
-		coef.resize(n);
-		for (int i = 0; i < real.size(); ++i) coef[i] = real[i];
-		for (int i = real.size(); i < n; ++i) coef[i] = complex<long double>(0);
-	}
-
-	explicit poly(int m) {
-		int n = next_pow_2(m);
-		coef.resize(n, complex<long double>(0));
-	}
-
-	size_t size() const {
-		return coef.size();
-	}
-
-	complex<long double> &operator[](int idx) {
-		return coef[idx];
-	}
-
-
-	const complex<long double> &operator[](int idx) const {
-		return coef[idx];
-	}
-
-	static size_t reverseBits(size_t num, int log2n) {
-		size_t reversed = 0;
-		for (int i = 0; i < log2n; ++i)
-			if (num & (1 << i)) reversed |= (1 << (log2n - 1 - i));
-		return reversed;
-	}
-
-	friend void fft(poly &a, bool invert = false) {
+	static void fft(vector<complex<U>> &a) {
 		int n = a.size();
-		int log2n = log2(n);
-		for (int i = 0; i < n; ++i) {
-			int j = reverseBits(i, log2n);
-			if (i < j) swap(a[i], a[j]);
-		}
-		for (int len = 2; len <= n; len *= 2) {
-			long double angle = 2 * acos(-1) / len * (invert ? -1 : 1);
-			complex<long double> wlen(cos(angle), sin(angle));
-			for (int i = 0; i < n; i += len) {
-				complex<long double> w(1);
-				for (int j = 0; j < len / 2; ++j) {
-					complex<long double> u = a[i + j];
-					complex<long double> v = a[i + j + len / 2] * w;
-					a[i + j] = u + v;
-					a[i + j + len / 2] = u - v;
-					w *= wlen;
+		static int last_n;
+		static vector<int> rev;
+		static vector<complex<U>> rt(2, 1);
+		if (n != last_n) {
+			rev.resize(n);
+			int L = 31 - __builtin_clz(n);
+			for (int i = 0; i < n; ++i) { rev[i] = (rev[i / 2] | (i & 1) << L) / 2; }
+			last_n = n;
+			int prev = rt.size();
+			rt.resize(n);
+			for (int k = prev; k < n; k *= 2) {
+				auto x = polar(1.0L, acos(-1.0L) / k);
+				for (int i = k; i < 2 * k; ++i) {
+					rt[i] = i & 1 ? rt[i / 2] * x : rt[i / 2];
 				}
 			}
 		}
-		if (invert) {
-			for (auto &x: a.coef) x /= n;
+
+		for (int i = 1; i < n; i++) { if (i < rev[i]) swap(a[i], a[rev[i]]); }
+		complex<U> *A = a.data();
+		for (int len = 1; len < n; len *= 2) {
+			for (int i = 0; i < n; i += 2 * len) {
+				for (int j = 0; j < len; ++j) {
+					complex<U> v = A[i + j + len] * rt[len + j];
+					A[i + j + len] = A[i + j] - v;
+					A[i + j] += v;
+				}
+			}
 		}
 	}
-
-	friend poly operator*(poly a, poly b) {
+	friend poly operator*(const poly &a, const poly &b) {
 		int n = next_pow_2(a.size() + b.size() - 1);
-		a.coef.resize(n);
-		b.coef.resize(n);
-		fft(a, false);
-		fft(b, false);
-		for (int i = 0; i < n; ++i) a[i] *= b[i];
-		fft(a, true);
-		a.coef.resize(a.size() + b.size() - 1);
-		for (auto &x: a.coef) x = complex<long double>(round(x.real()), 0);
-		return a;
+		vector<complex<U>> in(n), out(n);
+		copy(a.coef.begin(), a.coef.end(), in.begin());
+		for (int i = 0; i < b.size(); ++i) { in[i].imag(b[i]); }
+		fft(in);
+		for (int i = 0; i < n; ++i) { in[i] *= in[i]; }
+		for (int i = 0; i < n; ++i) { out[i] = in[-i & (n - 1)] - conj(in[i]); }
+		fft(out);
+		poly res(a.size() + b.size() - 1);
+		for (int i = 0; i < res.size(); ++i) { res.coef[i] = round(out[i].imag() / (4 * n)); }
+		return res;
 	}
 };
--- library/polynomial/polymod.h
+++ library/polynomial/polymod.h
@@ -1,90 +1,20 @@
 #pragma once
 
-template<int mod = 998244353, int root = 3>
+template<int mod = 998244353, int root = 3, int NMAX = 25>
 struct poly {
-	std::vector<int> coef;
-
+	vector<int> coef;
 	poly() {}
-
-	static int next_pow_2(int n) {
-		int p = 1;
-		while (p < n) p <<= 1;
-		return p;
+	static inline int next_pow_2(int n) { return 1 << (32 - __builtin_clz(n - 1)); }
+	template<typename V>
+	explicit poly(const vector<V> &real) {
+		coef.resize(real.size());
+		for (int i = 0; i < real.size(); ++i) { coef[i] = real[i]; }
 	}
-
-	explicit poly(int m) {
-		int n = next_pow_2(m);
-		coef.assign(n, 0);
-	}
-
-	template<typename T>
-	explicit poly(const std::vector <T> &real, int m = -1) {
-		if (m == -1) m = (int) real.size();
-		int n = next_pow_2(m);
-		coef.resize(n);
-		for (int i = 0, sz = (int) real.size(); i < sz; ++i)
-			coef[i] = real[i];
-		for (int i = (int) real.size(); i < n; ++i)
-			coef[i] = 0;
-	}
-
-	size_t size() const { return coef.size(); }
-
-	int &operator[](int idx) { return coef[idx]; }
-	const int &operator[](int idx) const { return coef[idx]; }
-
-	static size_t reverseBits(size_t num, int log2n) {
-		size_t rev = 0;
-		for (int i = 0; i < log2n; ++i)
-			if (num & (size_t(1) << i))
-				rev |= (size_t(1) << (log2n - 1 - i));
-		return rev;
-	}
-
-	friend void ntt(poly &a, bool invert = false) {
-		int n = (int) a.size(), log2n = __builtin_ctz(n);
-		std::vector<int> rev(n);
-		for (int i = 0; i < n; ++i)
-			rev[i] = (int) reverseBits(i, log2n);
-		for (int i = 0; i < n; ++i)
-			if (i < rev[i]) std::swap(a[i], a[rev[i]]);
-
-		for (int len = 2; len <= n; len <<= 1) {
-			int wlen = pow_mod(root, (mod - 1) / len);
-			if (invert) wlen = pow_mod(wlen, mod - 2);
-			for (int i = 0; i < n; i += len) {
-				int w = 1;
-				for (int j = 0; j < len / 2; ++j) {
-					int u = a[i + j];
-					int v = int(1LL * a[i + j + len / 2] * w % mod);
-					a[i + j] = u + v < mod ? u + v : u + v - mod;
-					a[i + j + len / 2] = u - v >= 0 ? u - v : u - v + mod;
-					w = int(1LL * w * wlen % mod);
-				}
-			}
-		}
-
-		if (invert) {
-			int inv_n = pow_mod((int) n, mod - 2);
-			for (auto &x: a.coef)
-				x = int(1LL * x * inv_n % mod);
-		}
-	}
-
-	friend poly operator*(poly a, poly b) {
-		int n = next_pow_2((int) a.size() + (int) b.size() - 1);
-		a.coef.resize(n);
-		b.coef.resize(n);
-		ntt(a, false);
-		ntt(b, false);
-		for (int i = 0; i < n; ++i)
-			a[i] = int(1LL * a[i] * b[i] % mod);
-		ntt(a, true);
-		a.coef.resize((int) a.size() + (int) b.size() - 1);
-		return a;
-	}
-
-	static int pow_mod(int base, int exp) {
+	explicit poly(int n) { coef.resize(n, 0); }
+	inline size_t size() const { return coef.size(); }
+	inline int &operator[](int idx) { return coef[idx]; }
+	inline const int &operator[](int idx) const { return coef[idx]; }
+	static inline int pow_mod(ll base, ll exp) {
 		int result = 1;
 		while (exp > 0) {
 			if (exp & 1) result = int(1LL * result * base % mod);
@@ -93,7 +23,79 @@ struct poly {
 		}
 		return result;
 	}
+	static void ntt(vector<int> &a) {
+		int n = a.size();
+		static int last_n;
+		static vector<int> rev;
+		static vector<ll> rt(2, 1);
+		if (n != last_n) {
+			rev.resize(n);
+			int L = 31 - __builtin_clz(n);
+			for (int i = 0; i < n; ++i) { rev[i] = (rev[i / 2] | (i & 1) << L) / 2; }
+			last_n = n;
+			if (rt.size() < n) {
+				int prev = rt.size();
+				rt.resize(n);
+				for (int k = prev, s = __builtin_ctz(prev) + 1; k < n; k *= 2, s++) {
+					auto x = pow_mod(root, mod >> s);
+					for (int i = k; i < 2 * k; ++i) {
+						rt[i] = i & 1 ? rt[i / 2] * x % mod : rt[i / 2];
+					}
+				}
+			}
+		}
 
+		for (int i = 1; i < n; i++) { if (i < rev[i]) swap(a[i], a[rev[i]]); }
+		int *A = a.data();
+		for (int len = 1; len < n; len *= 2) {
+			for (int i = 0; i < n; i += 2 * len) {
+				for (int j = 0; j < len; ++j) {
+					int u = A[i + j], v = A[i + j + len] * rt[len + j] % mod;
+					A[i + j] = u + v - (u + v >= mod ? mod : 0);
+					A[i + j + len] = u - v + (u < v ? mod : 0);
+				}
+			}
+		}
+	}
+	friend poly operator*(const poly &a, const poly &b) {
+		int s = (int) a.size() + (int) b.size() - 1, n = next_pow_2(s);
+		int inv = pow_mod(n, mod - 2);
+		vector<int> L(a.coef), R(b.coef), out(n);
+		L.resize(n), R.resize(n);
+		ntt(L), ntt(R);
+		for (int i = 0; i < n; ++i) {
+			out[-i & (n - 1)] = (ll) L[i] * R[i] % mod * inv % mod;
+		}
+		ntt(out);
+		poly res(s);
+		copy_n(out.begin(), s, res.coef.begin());
+		return res;
+	}
+	poly operator*=(const poly &b) {
+		int s = (int) size() + (int) b.size() - 1, n = next_pow_2(s);
+		int inv = pow_mod(n, mod - 2);
+		vector<int> L(coef), R(b.coef), out(n);
+		L.resize(n), R.resize(n);
+		ntt(L), ntt(R);
+		for (int i = 0; i < n; ++i) {
+			out[-i & (n - 1)] = (ll) L[i] * R[i] % mod * inv % mod;
+		}
+		ntt(out);
+		coef.resize(s);
+		for (int i = 0; i < s; ++i) {
+			coef[i] = out[i];
+		}
+		return *this;
+	}
+
+	poly &operator*=(ll k) {
+		k %= mod;
+		if (k < 0) k += mod;
+		for (int &x: coef) x = 1ll * x * k % mod;
+		return *this;
+	}
+	friend poly operator*(poly p, ll k) { return p *= k; }
+	friend poly operator*(ll k, poly p) { return p *= k; }
 
 	static void fwt_or(poly &a, bool invert) {
 		int n = (int) a.size();
@@ -144,7 +146,7 @@ struct poly {
 		if (invert) {
 			int inv_n = pow_mod(n, mod - 2);
 			for (auto &x: a.coef)
-				x = int(1LL * x * inv_n % mod);
+				x = 1ll * x * inv_n % mod;
 		}
 	}
 
@@ -160,8 +162,9 @@ struct poly {
 		f(a, false);
 		f(b, false);
 		for (int i = 0; i < n; ++i)
-			a.coef[i] = int(1LL * a.coef[i] * b.coef[i] % mod);
+			a.coef[i] = int(1ll * a.coef[i] * b.coef[i] % mod);
 		invf(a, true);
+		a.coef.resize(need);
 		return a;
 	}
 
@@ -175,5 +178,307 @@ struct poly {
 
 	friend poly convolution_xor(const poly &a, const poly &b) {
 		return fwt_conv(a, b, fwt_xor, fwt_xor);
+	}
+
+	poly operator+=(const poly &rhs) {
+		int min_sz = min(size(), rhs.size());
+		for (int i = 0; i < min_sz; ++i) {
+			coef[i] += rhs.coef[i];
+			if (coef[i] >= mod) coef[i] -= mod;
+		}
+		if (size() < rhs.size()) {
+			coef.resize(rhs.size());
+			for (int i = min_sz; i < rhs.size(); ++i) {
+				coef[i] = rhs.coef[i];
+			}
+		}
+		return *this;
+	}
+	poly operator-=(const poly &rhs) {
+		int min_sz = min(size(), rhs.size());
+		for (int i = 0; i < min_sz; ++i) {
+			coef[i] -= rhs.coef[i];
+			if (coef[i] < 0) {
+				coef[i] += mod;
+			}
+		}
+		if (size() < rhs.size()) {
+			coef.resize(rhs.size());
+			for (int i = size(); i < rhs.size(); ++i) {
+				coef[i] = -rhs.coef[i] + mod;
+			}
+		}
+		return *this;
+	}
+	friend poly operator+(poly lhs, const poly &rhs) {
+		return lhs += rhs;
+	}
+	friend poly operator-(poly lhs, const poly &rhs) {
+		return lhs -= rhs;
+	}
+	poly operator+=(ll k) {
+		coef[0] = ((coef[0] + k) % mod + mod) % mod;
+		return *this;
+	}
+	poly operator-=(ll k) {
+		coef[0] = ((coef[0] - k) % mod + mod) % mod;
+		return *this;
+	}
+	friend poly operator+(poly lhs, ll k) {
+		return lhs += k;
+	}
+	friend poly operator-(poly lhs, ll k) {
+		return lhs -= k;
+	}
+	friend poly operator-(ll k, poly rhs) {
+		return rhs * -1 + k;
+	}
+	poly inv() const {
+		poly cur_a(vector<ll>{coef[0]});
+
+		poly b(vector<ll>{pow_mod(coef[0], mod - 2)});
+		for (int k = 1; k < size(); k *= 2) {
+			for (int i = k; i < min(2 * k, size()); ++i) {
+				cur_a.coef.push_back(coef[i]);
+			}
+			b *= 2 - b * cur_a;
+			b.coef.resize(2 * k);
+		}
+		return b;
+	}
+
+	poly operator/(poly rhs) const {
+		return (*this) * rhs.inv();
+	}
+	poly operator%(poly rhs) {
+		return (*this) - ((*this) / rhs) * rhs;
+	}
+	poly log() const {
+		poly res = this->deriv() * this->inv();
+		res.coef.resize(size() - 1);
+		return res.integral();
+	}
+	void log_inplace() {
+		*this = this->deriv() * this->inv();
+		coef.resize(size() - 1);
+		integral_inplace();
+	}
+	poly exp() const {
+		if (size() == 1) {
+			return poly(vector<ll>{1});
+		}
+		poly cur_a(vector<ll>{coef[0], coef[1]});
+		poly b(vector<ll>{1});
+		for (int k = 1; k < size(); k *= 2) {
+			for (int i = 2 * k; i < min(4 * k, size()); ++i) {
+				cur_a.coef.push_back(coef[i]);
+			}
+			b *= (cur_a - b.log() + 1);
+			b.coef.resize(4 * k);
+		}
+		return b;
+	}
+
+	static inline bool euler_rule(int x) {
+		return x == 0 || pow_mod(x, (mod - 1) / 2) == 1;
+	}
+
+	static inline int mod_sqrt(int n) {
+		ll a, i2;
+		mt19937 engine(chrono::steady_clock::now().time_since_epoch().count());
+		while (true) {
+			a = uniform_int_distribution<ll>(0, mod)(engine);
+			i2 = ((a * a - n) % mod + mod) % mod;
+			if (!euler_rule(i2)) {
+				break;
+			}
+		}
+		int pow = (mod + 1) / 2;
+		pll ans = {1, 0};
+		pll base = {a, 1};
+		while (pow > 0) {
+			if (pow & 1)
+				ans = {(ans.first * base.first + (i2 * ((ans.second * base.second) % mod)) % mod) % mod,
+				       (ans.first * base.second + ans.second * base.first) % mod};
+			base = {(base.first * base.first + ((i2 * ((base.second * base.second) % mod)) % mod)) % mod,
+			        (2 * base.first * base.second) % mod};
+			pow >>= 1;
+		}
+		return ans.first;
+	}
+
+	poly sqrt() const {
+		int i = 0;
+		for (; i < size(); ++i) {
+			if (coef[i] != 0) {
+				break;
+			}
+		}
+		if (i == size()) {
+			return poly(vector<ll>(size(), 0));
+		}
+		if (i & 1) {
+			throw domain_error("poly::sqrt: leading coefficient is not a quadratic residue");
+		}
+		auto cur_coef = vector<ll>(coef.begin() + i, coef.end());
+		if (!euler_rule(cur_coef[0])) {
+			throw domain_error("poly::sqrt: leading coefficient is not a quadratic residue");
+		}
+		poly b(vector<ll>{mod_sqrt(cur_coef[0])});
+		if (cur_coef.size() > 1) {
+			poly cur_a(vector<ll>{cur_coef[0], cur_coef[1]});
+			int inv2 = (mod + 1) / 2;
+			for (int k = 1; k < size() - i / 2; k *= 2) {
+				for (int i = 2 * k; i < min(4 * k, cur_coef.size()); ++i) {
+					cur_a.coef.push_back(cur_coef[i]);
+				}
+				b += cur_a * b.inv();
+				b *= inv2;
+				b.coef.resize(4 * k);
+			}
+		}
+		poly ans(size());
+		for (int j = i / 2; j < size(); ++j) {
+			ans[j] = b[j - i / 2];
+		}
+		return ans;
+	}
+
+	void sqrt_inplace() {
+		int i = 0;
+		for (; i < size(); ++i) {
+			if (coef[i] != 0) {
+				break;
+			}
+		}
+		if (i == size()) {
+			coef.assign(size(), 0);
+		}
+		if (i & 1) {
+			throw domain_error("poly::sqrt: leading coefficient is not a quadratic residue");
+		}
+		auto cur_coef = vector<ll>(coef.begin() + i, coef.end());
+		if (!euler_rule(cur_coef[0])) {
+			throw domain_error("poly::sqrt: leading coefficient is not a quadratic residue");
+		}
+		poly b(vector<ll>{mod_sqrt(cur_coef[0])});
+		if (cur_coef.size() > 1) {
+			poly cur_a(vector<ll>{cur_coef[0], cur_coef[1]});
+			int inv2 = (mod + 1) / 2;
+			for (int k = 1; k < size() - i / 2; k *= 2) {
+				for (int i = 2 * k; i < min(4 * k, cur_coef.size()); ++i) {
+					cur_a.coef.push_back(cur_coef[i]);
+				}
+				b += cur_a * b.inv();
+				b *= inv2;
+				b.coef.resize(4 * k);
+			}
+		}
+		for (int j = 0; j < i / 2; ++j) {
+			coef[j] = 0;
+		}
+		for (int j = i / 2; j < size(); ++j) {
+			coef[j] = b[j - i / 2];
+		}
+	}
+
+	poly operator>>(int k) const {
+		return poly(vector<ll>(coef.begin() + k, coef.end()));
+	}
+	poly operator<<(int k) const {
+		vector<ll> new_coef(k, 0);
+		for (auto &c: coef) {
+			new_coef.push_back(c);
+		}
+		return poly(new_coef);
+	}
+	poly operator>>=(int k) {
+		return *this >> k;
+	}
+	poly operator<<=(int k) {
+		return *this << k;
+	}
+
+	poly pow(ll k) const {
+		if (k == 0) {
+			poly ans(vector<ll>(size(), 0));
+			ans[0] = 1;
+			return ans;
+		}
+		int i = 0;
+		for (; i < size(); ++i) {
+			if (coef[i] != 0) {
+				break;
+			}
+		}
+		if (i == size()) {
+			return poly(vector<ll>(size(), 0));
+		}
+
+		return (pow_mod(coef[i], k) * ((k % mod) * ((*this >> i).log())).exp()) << min(min(k, size()) * i, size());
+	}
+
+	void pow_inplace(ll k) {
+		if (k == 0) {
+			coef[0] = 1;
+		}
+		int i = 0;
+		for (; i < size(); ++i) {
+			if (coef[i] != 0) {
+				break;
+			}
+		}
+		if (i == size()) {
+			coef.assign(size(), 0);
+			return;
+		}
+		*this >>= i;
+		this->log_inplace();
+		*this *= k % mod;
+		*this = (this->exp() * pow_mod(coef[i], k)) << min(min(k, size()) * i, size());
+	}
+
+	poly deriv() const {
+		if (size() == 1) {
+			return poly(vector<int>{0});
+		}
+		poly res(size() - 1);
+		for (size_t i = 0; i < size() - 1; ++i) {
+			res.coef[i] = (1ll * coef[i + 1] * (i + 1)) % mod;
+		}
+		return res;
+	}
+
+	poly integral() const {
+		int n = size();
+		poly res(n + 1);
+		res.coef[0] = 0;
+		for (int i = 0; i < n; ++i) {
+			res.coef[i + 1] = (1ll * coef[i] * pow_mod(i + 1, mod - 2)) % mod;
+		}
+		return res;
+	}
+	void deriv_inplace() {
+		if (size() == 1) {
+			coef[0] = 0;
+			return;
+		}
+		for (int i = 0; i < size() - 1; ++i) {
+			coef[i] = (1ll * coef[i + 1] * (i + 1)) % mod;
+		}
+		coef.resize(size() - 1);
+	}
+	void integral_inplace() {
+		coef.resize(size() + 1);
+		for (int i = size() - 1; i >= 1; --i) {
+			coef[i] = (1ll * coef[i - 1] * pow_mod(i, mod - 2)) % mod;
+		}
+		coef[0] = 0;
+	}
+	bool operator==(const poly &rhs) const {
+		return coef == rhs.coef;
+	}
+	bool operator!=(const poly &rhs) const {
+		return !(rhs == *this);
 	}
 };
