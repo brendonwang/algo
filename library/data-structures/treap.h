d2acc73c260d8ffaab610b06f2b1f9465de4e722
brendon.wang <brendon.wang@gmail.com>
2026-07-21 22:03:31 -0700

    update treap

--- library/data-structures/treap.h
+++ library/data-structures/treap.h
@@ -1,233 +1,191 @@
-template<typename T, int MAXN = 1e6, typename lazy_tag = T>
+// NOTE: Current implementation does not work with generic f for now.
+template <typename T, typename L, typename F, typename A, typename LA>
 struct treap {
-	struct Node {
-		Node *l = nullptr, *r = nullptr;
-		T val, subtree;
-		unsigned int priority;
-		lazy_tag lazy;
-		int sz = 1;
-		bool rev = false;
-		Node() = default;
-		template<typename Treap>
-		inline void propagate(Treap *tr) {
-			if (lazy != tr->lazy_unit) {
-				tr->add(val, lazy, 1);
-				tr->add(subtree, lazy, sz);
-				if (l) tr->lazy_add(l->lazy, lazy);
-				if (r) tr->lazy_add(r->lazy, lazy);
-				lazy = tr->lazy_unit;
-			}
-			if (rev) {
-				swap(l, r);
-				if (l) l->rev = !l->rev;
-				if (r) r->rev = !r->rev;
-				rev = false;
-			}
-		}
+  struct Node {
+    T v;
+    T subtree_val;
+    L lazy;
+    bool rev = false;
+    int sz = 1;
+    Node *c[2] = {nullptr, nullptr};
 
-		template<typename Treap>
-		inline void update(Treap *tr) {
-			sz = 1;
-			subtree = val;
-			if (l) {
-				l->propagate(tr);
-				sz += l->sz;
-				subtree = tr->f(subtree, l->subtree);
-			}
-			if (r) {
-				r->propagate(tr);
-				sz += r->sz;
-				subtree = tr->f(subtree, r->subtree);
-			}
-		}
-	};
+    Node(const T &val, const L &lazy) : v(val), subtree_val(val), lazy(lazy) {}
+  };
 
-	mt19937 rng{(unsigned int) (std::chrono::steady_clock::now().time_since_epoch().count())};
+  inline T v(Node *x) { return (x == nullptr ? unit : x->v); }
 
-	static inline Node buff[MAXN];
-	static inline int cnt = 0;
+  inline L lazy(Node *x) { return (x == nullptr ? lazy_unit : x->lazy); }
 
-	inline Node *new_node(T val) {
-		assert(cnt < MAXN);
-		buff[cnt].val = val;
-		buff[cnt].subtree = val;
-		buff[cnt].lazy = lazy_unit;
-		buff[cnt].priority = rng();
+  inline bool rev(Node *x) { return (x == nullptr ? false : x->rev); }
 
-		return &buff[cnt++];
-	}
+  inline T subtree_val(Node *x) {
+    return (x == nullptr ? unit : x->subtree_val);
+  }
 
-	using combine_fn = T(*)(T, T);
-	using apply_fn = void (*)(T &, lazy_tag, int);
-	using lazy_apply_fn = void (*)(lazy_tag &, lazy_tag);
-	combine_fn f;
-	apply_fn add;
-	static inline void default_lazy_add(lazy_tag &a, lazy_tag b) { a += b; }
-	lazy_apply_fn lazy_add;
+  inline int sz(Node *x) { return (x == nullptr ? 0 : x->sz); }
 
-	T unit;
-	lazy_tag lazy_unit;
-	Node *root;
+  inline void pull(Node *x) {
+    if (!x)
+      return;
+    x->subtree_val = f(f(subtree_val(x->c[0]), x->v), subtree_val(x->c[1]));
+    x->sz = 1 + sz(x->c[0]) + sz(x->c[1]);
+  }
 
-	treap(T unit_, combine_fn f_, apply_fn add_, lazy_tag lazy_unit_, lazy_apply_fn lazy_add_ = default_lazy_add,
-	      Node *r = nullptr) : f(f_), add(add_), lazy_add(lazy_add_), unit(unit_), lazy_unit(lazy_unit_), root(r) {}
-	treap(T unit_, combine_fn f_, apply_fn add_, lazy_apply_fn lazy_add_ = default_lazy_add, Node *r = nullptr)
-			: f(f_), add(add_), lazy_add(lazy_add_), unit(unit_), lazy_unit(unit_), root(r) {}
-	treap(T unit_, combine_fn f_, Node *r = nullptr) : f(f_), unit(unit_), root(r) {}
+  inline void apply(Node *x, const L &lz, bool rev) {
+    if (!x)
+      return;
+    if (lz != lazy_unit) {
+      add(x->v, lz, 1);
+      add(x->subtree_val, lz, x->sz);
+      lazy_add(x->lazy, lz);
+    }
+    if (rev) {
+      x->rev ^= rev;
+      swap(x->c[0], x->c[1]);
+    }
+  }
 
-	inline int size(Node *x) {
-		if (x) {
-			return x->sz;
-		} else {
-			return 0;
-		}
-	}
+  inline void propagate(Node *x) {
+    if (!x || (x->lazy == lazy_unit && !x->rev))
+      return;
+    apply(x->c[0], x->lazy, x->rev);
+    apply(x->c[1], x->lazy, x->rev);
+    x->lazy = lazy_unit;
+    x->rev = false;
+  }
 
-	Node *find(int idx) {
-		Node *x = root;
-		while (x) {
-			x->propagate(this);
-			if (idx == size(x->l)) {
-				break;
-			} else if (size(x->l) < idx) {
-				if (x->r) {
-					idx -= size(x->l) + 1;
-					x = x->r;
-				} else {
-					break;
-				}
-			} else {
-				if (x->l) {
-					x = x->l;
-				} else {
-					break;
-				}
-			}
-		}
-		if (x) {
-			int sz = size(x->l);
-			if (sz == idx) {
-				return x;
-			} else {
-				return nullptr;
-			}
-		} else {
-			return nullptr;
-		}
-	}
-	pair<Node *, Node *> split(int idx, Node *x) {
-		if (x) {
-			x->propagate(this);
-			if (size(x->l) <= idx) {
-				auto [l, r] = split(idx - size(x->l) - 1, x->r);
-				x->r = l;
-				x->update(this);
-				return {x, r};
-			} else {
-				auto [l, r] = split(idx, x->l);
-				x->l = r;
-				x->update(this);
-				return {l, x};
-			}
-		} else {
-			return {nullptr, nullptr};
-		}
-	}
-	Node *merge(Node *l, Node *r) {
-		if (!l) return r;
-		if (!r) return l;
-		l->propagate(this);
-		r->propagate(this);
-		if (l->priority < r->priority) {
-			r->l = merge(l, r->l);
-			r->update(this);
-			return r;
-		} else {
-			l->r = merge(l->r, r);
-			l->update(this);
-			return l;
-		}
-	}
+  inline treap new_root(Node *x) {
+    return treap(unit, f, add, lazy_unit, lazy_add, x);
+  }
 
-	void insert(int idx, T val) {
-		if (!root) {
-			root = new_node(val);
-			return;
-		}
-		auto [left, right] = split(idx, root);
-		Node *new_val = new_node(val);
-		root = merge(merge(left, new_val), right);
-	}
-	Node *erase(int idx, Node *x) {
-		if (!x) return nullptr;
-		x->propagate(this);
-		if (size(x->l) == idx) {
-			return merge(x->l, x->r);
-		} else if (size(x->l) < idx) {
-			x->r = erase(idx - size(x->l) - 1, x->r);
-		} else {
-			x->l = erase(idx, x->l);
-		}
-		x->update(this);
-		return x;
-	}
-	void erase(int idx) {
-		root = erase(idx, root);
-	}
-	template<typename F>
-	void change(int idx, T val, F apply) {
-		auto [left, right] = split(idx - 1, root);
-		auto [mid, rest] = split(0, right);
-		if (mid) {
-			mid->val = apply(mid->val, val);
-			mid->update(this);
-		}
-		root = merge(merge(left, mid), rest);
-	}
-	template<typename F>
-	void change(int l, int r, lazy_tag val, F apply) {
-		auto [left, right] = split(l - 1, root);
-		auto [mid, rest] = split(r - l, right);
-		if (mid) {
-			apply(mid->lazy, val);
-			mid->propagate(this);
-			mid->update(this);
-		}
-		root = merge(merge(left, mid), rest);
-	}
-	void update(int idx, T val) {
-		change(idx, val, [](T a, T b) { return a + b; });
-	}
-	void set(int idx, T val) {
-		change(idx, val, [](T a, T b) { return b; });
-	}
-	void update(int l, int r, lazy_tag val) {
-		change(l, r, val, [](lazy_tag &a, lazy_tag b) { a += b; });
-	}
-	void set(int l, int r, lazy_tag val) {
-		change(l, r, val, [](lazy_tag &a, lazy_tag b) { a = b; });
-	}
+  static uint64_t rng() {
+    static uint64_t x = chrono::steady_clock::now().time_since_epoch().count();
 
-	void apply(int l, int r, lazy_tag val) {
-		change(l, r, val, lazy_add);
-	}
+    x += 0x9e3779b97f4a7c15ULL;
+    uint64_t z = x;
+    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9ULL;
+    z = (z ^ (z >> 27)) * 0x94d049bb133111ebULL;
+    return z ^ (z >> 31);
+  }
 
-	T query(int l, int r) {
-		auto [left, right] = split(l - 1, root);
-		auto [mid, rest] = split(r - l, right);
-		T ans = mid ? mid->subtree : unit;
-		root = merge(merge(left, mid), rest);
-		return ans;
-	}
-	void reverse(int l, int r) {
-		auto [left, right] = split(l - 1, root);
-		auto [mid, rest] = split(r - l, right);
-		if (mid) {
-			mid->rev = true;
-			mid->propagate(this);
-			mid->update(this);
-		}
-		root = merge(merge(left, mid), rest);
-	}
+  T unit;
+  F f;
+  A add;
+  L lazy_unit;
+  LA lazy_add;
+
+  Node *root;
+
+  template <class X = T, class Y = L,
+            std::enable_if_t<std::is_same<X, Y>::value, int> = 0>
+  treap(const T &unit, const F &f, const A &add, const LA &lazy_add,
+        Node *r = nullptr)
+      : f(f), unit(unit), add(add), lazy_unit(unit), lazy_add(lazy_add),
+        root(r) {}
+
+  treap(const T &unit, const F &f, const A &add, const L &lazy_unit,
+        const LA &lazy_add, Node *r = nullptr)
+      : f(f), unit(unit), add(add), lazy_unit(lazy_unit), lazy_add(lazy_add),
+        root(r) {}
+  Node *find(int idx) {
+    Node *x = root, *last = nullptr;
+    while (x) {
+      propagate(x);
+
+      last = x;
+      if (sz(x->c[0]) == idx) {
+        return x;
+      }
+      if (idx < sz(x->c[0]))
+        x = x->c[0];
+      else {
+        idx -= sz(x->c[0]) + 1;
+        x = x->c[1];
+      }
+    }
+    return nullptr;
+  }
+
+  pair<Node*, Node*> split(Node* x, int idx) {
+    if (!x)
+      return {nullptr, nullptr};
+    propagate(x);
+    if (idx < sz(x->c[0])) {
+      auto [l, r] = split(x->c[0], idx);
+      x->c[0] = r;
+      pull(x);
+      return {l, x};
+    } else {
+      auto [l, r] = split(x->c[1], idx - sz(x->c[0]) - 1);
+      x->c[1] = l;
+      pull(x);
+      return {x, r};
+    }
+  }
+
+  Node* merge(Node* l, Node* r) {
+    if (!l) return r;
+    if (!r) return l;
+    propagate(l);
+    propagate(r);
+    if (((__uint128_t)rng() * (sz(l) + sz(r)) >> 64) < sz(l)) {
+      l->c[1] = merge(l->c[1], r);
+      pull(l);
+      return l;
+    } else {
+      r->c[0] = merge(l, r->c[0]);
+      pull(r);
+      return r;
+    }
+  }
+
+  void insert(int idx, const T &val) {
+    auto [l, r] = split(root, idx - 1);
+    Node* x = new Node(val, lazy_unit);
+    root = merge(l, merge(x, r));
+    pull(root);
+  }
+
+  array<Node*, 3> split(int l, int r) {
+    auto [left, midright] = split(l - 1);
+    auto [mid, right] = midright.split(r - l);
+    return {left, mid, right};
+  }
+
+  void erase(int idx) {
+    auto [l, x, r] = split(idx, idx + 1);
+    root = merge(l, r);
+  }
+
+  T query(int l, int r) {
+    auto [left, mid, right] = split(l, r);
+    T ans = subtree_val(mid);
+    root = merge(left, merge(mid, right));
+    return ans;
+  }
+
+  template <typename APPLY> void change(int idx, const T &val, APPLY apply) {
+    Node *x = find(idx); // TODO: Check
+    if (x) {
+      propagate(x);
+      apply(x->v, val);
+      pull(x);
+    }
+  }
+
+  void apply(int l, int r, const L &lz) {
+    auto [left, mid, right] = split(l, r);
+    if (mid) {
+      apply(mid, lz, false);
+    }
+    root = merge(left, merge(mid, right));
+  }
+
+  void reverse(int l, int r) {
+    auto [left, mid, right] = split(l, r);
+    if (mid) {
+      apply(mid, lazy_unit, true);
+    }
+    root = merge(left, merge(mid, right));
+  }
 };
-
